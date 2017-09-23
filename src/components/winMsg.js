5d52559647cc5759980a79dfb4ec1d235c69fcec
Hui Jiang <huijiang@Huis-MBP.home>
2017-09-23 01:56:56 -0400

    updated img size on win message component

--- src/components/winMsg.js
+++ src/components/winMsg.js
@@ -5,12 +5,9 @@ const WinMsg = ({msg})  => {
 	return (
 		<div>
 			<h1>Thank you for {msg}!!!</h1>
-			<img style={{width: 820, display: 'inline-block'}} src='https://s3.us-east-2.amazonaws.com/r3-village/won.jpg' alt='secret' />
+			<img style={{width: window.innerWidth*.8, display: 'inline-block'}} src='https://s3.us-east-2.amazonaws.com/r3-village/won.jpg' alt='secret' />
 		</div>
 	);
 };
 
 export default WinMsg;
-
-// var renderGames = <div><h1>"YOU WON!"</h1><img style={{width: 820, display: 'inline-block'}} src='https://media.giphy.com/media/wl6l9trsOaktq/giphy.gif' alt='firework' /></div>;
-		
