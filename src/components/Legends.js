37d5a84121a6b7f31824eee1102c218e4c9d1b50
Hui Jiang <huijiang@Huis-MBP.home>
2017-10-03 23:34:40 -0400

    updated icons page to show svg icon only

--- src/components/Legends.js
+++ src/components/Legends.js
@@ -8,26 +8,24 @@ import {
   TableRowColumn,
 } from 'material-ui/Table';
 import ContentDeleteSweep from 'material-ui/svg-icons/content/delete-sweep';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import AvReplay from 'material-ui/svg-icons/av/replay';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import Badge from 'material-ui/Badge';
+import {pink300, blue300} from 'material-ui/styles/colors';
 
 const Legends = () => {
 	var legends = {
-		learnGame: {p: 0, h: 5, icon: <ActionFavorite />, note: 'Reduce', secondary: false}, //reduce
-		matchGame: {p: 0, h: 5, icon: <ActionFavoriteBorder />, note: 'Reuse', secondary: false}, //reuse
-		recycleGame: {p: 3, h: 3, icon: <AvReplay />, note: "Recycle", secondary: false}, //recycle
-		addToy: {p: 3, h: 3, icon: <ContentAdd />, note: "Add a Toy", secondary: true},
-		Trash: {p: 5, h: 0, icon: <ContentDeleteSweep />, note: "Trash", secondary: true}};
+		learnGame: {p: 0, h: 5, icon: <ActionFavorite color={blue300} />, note: 'Reduce', secondary: false}, //reduce
+		matchGame: {p: 0, h: 5, icon: <ActionFavoriteBorder color={blue300} />, note: 'Reuse', secondary: false}, //reuse
+		recycleGame: {p: 3, h: 3, icon: <AvReplay color={blue300} />, note: "Recycle", secondary: false}, //recycle
+		addToy: {p: 3, h: 3, icon: <ContentAdd  color={pink300} />, note: "Add a Toy", secondary: true},
+		Trash: {p: 5, h: 0, icon: <ContentDeleteSweep color={pink300} />, note: "Trash", secondary: true}};
 
 	var renderContents = Object.keys(legends).map((key, index) => {
 		return <TableRow key={index}>
-					<TableRowColumn><FloatingActionButton mini={true} secondary={legends[key].secondary}>
-									{legends[key].icon}
-							</FloatingActionButton></TableRowColumn>
+					<TableRowColumn>{legends[key].icon}</TableRowColumn>
 					<TableRowColumn>{legends[key].note}</TableRowColumn>
 					<TableRowColumn><Badge primary={true} badgeContent={legends[key].h} ></Badge></TableRowColumn>
 					<TableRowColumn><Badge secondary={true} badgeContent={legends[key].p} ></Badge></TableRowColumn>
